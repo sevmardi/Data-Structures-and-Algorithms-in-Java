c631e276cc9a44f08db07c98593c96916e67ca8c
sm2011 <23eagle@live.com>
2014-06-26 22:48:28 +0200

    stackimplementation

--- Wk4/PostFixCalculator.java
+++ Wk4/PostFixCalculator.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+import Stack.ListStack;
+
 public class PostFixCalculator
 {
    private String invoerString;
@@ -18,12 +20,15 @@ public class PostFixCalculator
    
    private void evaluate()
    {
-     Stack mystack = new ListStack();
+     ListStack mystack = new ListStack();
+     
      StringTokenizer st = new StringTokenizer(invoerString);
+     
      while (st.hasMoreTokens())
      {
        String token = st.nextToken();
-       char eerste = ................ // het eerste karakter van elk token 
+       char eerste = 					// het eerste karakter van elk token 
+    		   
        if (( eerste >= '0') && (eerste <= '9'))
          ..............  // zie algoritme 
        else
@@ -43,7 +48,7 @@ public class PostFixCalculator
          ....... // wat doe je met het resultaat? zie algoritme
        }
      }
-     resultaat = ......... // als alles goed is gegaan resultaat
-   }                       // poppen en omzetten naar een int
+     resultaat = ......... 	// als alles goed is gegaan resultaat
+   }                       	// poppen en omzetten naar een int
    
 }
