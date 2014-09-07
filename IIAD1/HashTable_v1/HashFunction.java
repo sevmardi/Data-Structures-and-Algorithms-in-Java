7f1636f1c94894200ca7ce684f64025f92bad530
sm2011 <23eagle@live.com>
2014-09-07 17:13:17 +0200

    hash

---
+++ IIAD1/HashTable_v1/HashFunction.java
@@ -0,0 +1,23 @@
+package HashTable_v1;
+
+public class HashFunction {
+
+	String[] arr;
+	int arraySize;
+	int itemInArr = 0;
+	
+	
+	public static void main(String[] args) {
+		
+		HashFunction func = new HashFunction();
+		
+		HashTab;e balance = new HashTable();
+		String str;
+		D
+	}
+	public HashFunction() {
+		
+		 
+	}
+
+}
