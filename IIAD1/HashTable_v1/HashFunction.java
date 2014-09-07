156562ac12ed99f37ea247553888d0d137a291a3
sm2011 <23eagle@live.com>
2014-09-07 20:16:45 +0200

    Hashtable v.01

--- IIAD1/HashTable_v1/HashFunction.java
+++ IIAD1/HashTable_v1/HashFunction.java
@@ -1,23 +1,45 @@
 package HashTable_v1;
 
+import java.util.Enumeration;
+import java.util.Hashtable;
+
 public class HashFunction {
 
 	String[] arr;
 	int arraySize;
 	int itemInArr = 0;
-	
-	
-	public static void main(String[] args) {
-		
-		HashFunction func = new HashFunction();
-		
-		HashTab;e balance = new HashTable();
-		String str;
-		D
-	}
-	public HashFunction() {
-		
-		 
-	}
 
+	public static void main(String[] args) {
+
+		//HashFunction balance = new HashFunction();
+
+		Hashtable balance = new Hashtable();
+
+		String str;
+		Enumeration names;
+		double bal;
+		
+		balance.put("Zara", new Double(3434.34));
+		balance.put("Mahnaz", new Double(123.22));
+		balance.put("Ayan", new Double(1378.00));
+		balance.put("Daisy", new Double(99.22));
+		balance.put("Qadir", new Double(-19.08));
+
+		// show all balances in hash tables
+		
+		names = balance.keys();
+		while (names.hasMoreElements()) 
+		{
+			str = (String) names.nextElement();
+			System.out.println(str + ":" + balance.get(str));
+		}
+		System.out.println();
+		// depost 1,000 into Zara's account
+		// something wrong with this.
+		bal = ((Double)balance.get("Zara")).doubleValue();
+		
+		balance.put("Zara", new Double(bal + 1000));
+		System.out.println("Zara's new Balance: " + balance.get("Zara"));
+
+	}
 }
