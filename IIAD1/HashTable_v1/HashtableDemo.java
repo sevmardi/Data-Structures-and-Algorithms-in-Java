74f8fbd917d490603f58c807b993058fb0a256c1
sm2011 <23eagle@live.com>
2014-10-27 10:58:28 +0100

    hashtable

--- IIAD1/HashTable_v1/HashtableDemo.java
+++ IIAD1/HashTable_v1/HashtableDemo.java
@@ -1,10 +1,37 @@
 package HashTable_v1;
 
+import java.util.Enumeration;
+import java.util.Hashtable;
+
 public class HashtableDemo {
 	
 	public static void main(String[] args) {
 		
 		
+		Hashtable hash = new Hashtable();
 		
+		Enumeration names;
+		String str;
+		Double bal;
+		
+		hash.put("Zara", new Double(2323.23));
+		hash.put("Mahbna", new Double(123.23));
+		
+		// show all the values 
+		names = hash.keys();
+		while(names.hasMoreElements())
+			
+		{
+			str = (String)names.nextElement();
+			System.out.println(str+":" + hash.get(str));
+			hash.get(str);
+			
+		}
+		System.out.println();
+		// depost some money 
+		bal = ((Double)hash.get("Zara")).doubleValue();
+		hash.put("Zara", new Double(bal+100));
+		
+		hash.get("zara");
 	}
 }
---
+++ IIAD1/Hashtable_v3/Hash.java
@@ -0,0 +1,36 @@
+package Hashtable_v3;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class Hash 
+{
+	public static void main(String[] args) throws Exception
+	{
+		
+		// start with ten, expand by ten when limit reached
+		Hashtable hash = new Hashtable(10,10);
+
+		for(int i = 0 ; i<= 0;i++)
+		{
+			Integer integer = new Integer(i);
+			hash.put(integer, "Number : " + i);
+		}
+		
+		// get the value out again
+		System.out.println(hash.get(new Integer(5)));
+		
+		// get the value out again 
+		System.out.println(hash.get(new Integer(21)));
+		
+		System.in.read();
+		
+		// get all values 
+		
+		for(Enumeration e = hash.keys(); e.hasMoreElements();)
+		{
+			System.out.println(hash.get(e.nextElement()));
+		}
+		
+	}
+}
