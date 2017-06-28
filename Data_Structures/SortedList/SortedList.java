76c43dbdc6a655e1f36306d918f7d28128fc96b0
Sevak Mardirosian <smardirosians@gmail.com>
2017-06-28 16:59:53 +0200

    Implmented sortedlist

--- Data_Structures/SortedList/SortedList.java
+++ Data_Structures/SortedList/SortedList.java
@@ -2,7 +2,7 @@ package SortedList;
 
 public class SortedList {
 
-	private Link first;
+	private Link first; // ref to first item in the list 
 
 	public SortedList() {
 		first = null;
@@ -15,16 +15,48 @@ public class SortedList {
 	
 	public void insert(long key)
 	{
+		Link newLink = new Link(key); // make new link
+		Link previous = null; // start at first 
+		Link current = first;  
 		
+		// until end of list
+		while(current != null && key > current.dData)
+		{
+			previous = current;
+			current = current.next;
+		}
+		
+		// at the beginning of list
+		if(previous == null)
+		{ 
+			first = newLink; // first --> newLink;
+		}
+		else
+			previous.next = newLink;
+		
+		newLink.next = current;
 	}
 	
 	public Link remove()
 	{
+		Link temp = first; // save first 
+		first =	first.next; // delete first
 		
+		return temp; // return value
 	}
 	
 	public void displayList()
 	{
-			
+		System.out.println("List (first --> last): ");
+		
+		Link current = first; // start at the beginning of list
+		
+		while(current != null)
+		{
+			current.display();
+			current = current.next;
+		}
+		
+		System.out.println("");
 	}
 }
--- Data_Structures/SortedList/SortedListApp.java
+++ Data_Structures/SortedList/SortedListApp.java
@@ -4,5 +4,25 @@ public class SortedListApp {
 
 	public static void main(String[] args) {
 		
+		SortedList list = new SortedList();
+		
+		list.insert(12);
+		list.insert(90);
+		
+		list.displayList();
+		
+		
+		list.insert(10);
+		
+		list.insert(50);
+		list.insert(60);
+		
+		
+		list.displayList();
+		
+		list.remove();
+		
+		list.displayList();
+		
 	}
 }
